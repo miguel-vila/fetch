7ef66e11485540c579afee4fa11fb1a33d4edecb
miguel <miguelvilag@gmail.com>
2015-04-26 22:50:32 -0500

    Simplificación

--- src/main/scala/saxl/DataSource.scala
+++ src/main/scala/saxl/DataSource.scala
@@ -5,6 +5,6 @@ import scala.concurrent.{ExecutionContext, Future}
 /**
  * Created by mglvl on 25/04/15.
  */
-abstract class DataSource[R[_]:Request] {
+abstract class DataSource[R[_]] {
   def fetch(blockedRequests: Seq[BlockedRequest[R,_]])(implicit executionContext: ExecutionContext): Future[Unit]
 }
