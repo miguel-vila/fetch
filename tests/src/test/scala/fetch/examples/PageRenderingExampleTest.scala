c360965d8a23023a39f43951244bcbf8b0807f6b
miguel <miguelvilag@gmail.com>
2015-05-05 22:52:42 -0500

    agrega patience

--- tests/src/test/scala/fetch/examples/PageRenderingExampleTest.scala
+++ tests/src/test/scala/fetch/examples/PageRenderingExampleTest.scala
@@ -2,6 +2,7 @@ package fetch.examples
 
 import fetch.{ FetchSuccess, FetchStatus, DataCache, Fetch }
 import fetch.examples.page_rendering.PageRenderingExample
+import org.scalatest.time.{ Seconds, Millis, Span }
 import org.scalatest.{ Matchers, WordSpec }
 import org.scalatest.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -10,6 +11,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * Created by mglvl on 5/05/15.
  */
 class PageRenderingExampleTest extends WordSpec with ScalaFutures with PageRenderingExample with Matchers {
+
+  val patience =
+    PatienceConfig(timeout = Span(1, Seconds), interval = Span(15, Millis))
 
   "PageRenderingExample" must {
 
@@ -36,7 +40,7 @@ class PageRenderingExampleTest extends WordSpec with ScalaFutures with PageRende
           }
           val cacheSize = 1 /*post ids fetch*/ + postInfoData.size + postViewsData.size + 5 /*los 5 de PostContent*/
           cache.size shouldEqual (cacheSize)
-      }
+      }(patience)
     }
 
     "when replaying with the same cache it just reruns without making any fetch" in {
@@ -52,7 +56,7 @@ class PageRenderingExampleTest extends WordSpec with ScalaFutures with PageRende
           html1 shouldEqual (html2) //Results are the same when replaying
           cache1 shouldEqual (cache2) //Caches should be the same when replaying
           stats2.numberOfRounds shouldEqual (0) //Number of rounds should be zero when replaying
-      }
+      }(patience)
     }
 
   }
